bce8ad49c14e791e6558e74c017b8f87768a1afb
zaknisdev <zakianisf1710@gmail.com>
2025-05-15 11:19:10 +0700

    mengubah node pertama menjadi current dan mengulangi selama current tidak null

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -113,5 +113,22 @@ class DoubleLinkedList
         delete current;
         cout << "Record with roll number " << rollNo << " deleted " << endl;
     }
-    
+
+    void traverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        node *currentNode = START;
+
+        cout << "\nReccord in ascending order of roll number are:\n";
+        int i = 0
+        while (currentNode != NULL)
+        {
+            
+        }
+    }
 };
