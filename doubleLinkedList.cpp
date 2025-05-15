d2f9e3609cdffea66ce350d686f62f1d1ef522d8
zaknisdev <zakianisf1710@gmail.com>
2025-05-15 10:28:43 +0700

    membuat class DoubleLinkedList

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -10,3 +10,22 @@ class node
     node *prev;
 };
 
+class DoubleLinkedList
+{
+    private:
+    node *Start;
+
+    public:
+    DoubleLinkedList()
+    {
+        Start = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        string nim;
+        cout << "\nEnter the roll number of the student";
+        cin >> nim;
+    }
+};
